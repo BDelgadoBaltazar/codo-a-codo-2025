2584b72dc71c072235f1ebd864c9e07f6af7bd84
BDelgadoBaltazar <baltidelgado@gmail.com>
2025-06-12 18:29:24 -0300

    Agrego scope, arrays, objetos y funciones

---
+++ JS/src/arrays.js
@@ -0,0 +1,20 @@
+// Creación
+//                  0  1  2   3  4
+let nota_alumnos = [7, 8, 9, 10, 8];
+
+// formas de acceso
+console.log(nota_alumnos);
+console.log("Primero: " + nota_alumnos[0]); // Obtengo el primero
+console.log("Ultimo: " + nota_alumnos[nota_alumnos.length - 1]); // Obtengo el último
+
+// longitud
+console.log(nota_alumnos.length);
+
+// sort
+
+// modificación
+
+// Agregar y quitar
+
+// recorrer
+
