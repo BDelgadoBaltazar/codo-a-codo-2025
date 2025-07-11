3a348206efd99cb3d9adf8d5f169a6f77f7eee74
BDelgadoBaltazar <baltidelgado@gmail.com>
2025-07-11 18:01:41 -0300

    Agrego contenido de DOM, APIS y json

---
+++ JS/src/dom_eventos.js
@@ -0,0 +1,36 @@
+let original = document.querySelector("#Original");
+let contenedor = document.querySelector("#Contenedor");
+
+console.log(original);
+console.log(contenedor);
+
+let botonAgregar = document.querySelector("#Agregar");
+let botonQuitar = document.querySelector("#Quitar");
+
+original.querySelector("h2").innerHTML = "Este titulo fue modificado por JS";
+
+let copia = original.cloneNode(true);
+
+original.remove();
+
+copia.style.backgroundColor = '#f00';
+copia.style.padding = "15px";
+
+function AgregarArticulo() {
+    contenedor.appendChild(copia.cloneNode(true));
+}
+
+function QuitarArticulo() {
+    if(contenedor.childElementCount > 0){
+        contenedor.removeChild(contenedor.lastChild);
+    }
+}
+
+// Eventos
+botonAgregar.addEventListener("click", function() {
+    AgregarArticulo();
+});
+
+botonQuitar.addEventListener("click", function() {
+    QuitarArticulo();
+});
---
+++ JS/src/json.js
@@ -0,0 +1,69 @@
+// Creo un objeto (tambien llamado array asociativo | en python se los llama diccionarios)
+// Los elementos no están ordenados
+// JSON Javascript Object Notation
+
+let persona = {
+    nombre: "Carlos",
+    apellido: "Lopez",
+    dni: 1234,
+    cuota_al_dia: true,
+
+    curso: {
+        turno: "Mañana",
+        comision: 24264,
+        nombre: "Fullstack Python",
+        notas: [7,8,9,10]
+    },
+
+    titulo: null,
+
+    mejores_amigos: ["Maria", "Gaston"]
+};
+
+// API - Application Programming Interface
+// Consumiendo una API
+
+fetch("https://randomuser.me/api")
+.then(response => response.json())
+.then(data => {
+    // Procesamiento de la info que llega de la API
+    console.log(data);
+    })
+.catch(error => console.log("Ocurrio un error!" + error));
+
+
+
+/* fuentes
+Fetch:
+- https://developer.mozilla.org/es/docs/Web/API/Fetch_API/Using_Fetch
+
+GET a un repo con github pages:
+- https://medium.com/@peternjuguna76/hosting-a-json-file-on-github-pages-a-step-by-step-guide-52105a5a393a
+- https://stackoverflow.com/questions/39199042/serve-json-data-from-github-pages
+
+APIS
+(Movies) https://developer.themoviedb.org/docs/getting-started
+(Harry Potter) - Suspendida - https://hp-api.onrender.com/
+(Pokemon) https://pokeapi.co/
+(Listado de APIS) https://github.com/public-apis/public-apis
+(Dragon Ball API) https://web.dragonball-api.com/
+(Random user) https://randomuser.me/api
+*/
+
+// https://username.github.io/reponame/file.json
+
+fetch("https://lucianopueyo.github.io/Codo-a-codo-Fullstack-24172/datos.json")
+.then(response => response.json())
+.then(data => {
+    // Procesamiento de la info que llega de la API
+    console.log(data);
+    })
+.catch(error => console.log("Ocurrio un error!" + error));
+
+fetch("https://dragonball-api.com/api/characters/1")
+.then(response => response.json())
+.then(data => {
+    // Procesamiento de la info que llega de la API
+    console.log(data);
+    })
+.catch(error => console.log("Ocurrio un error!" + error));
